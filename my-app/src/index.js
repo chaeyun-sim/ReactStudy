acf4d62fc2a9105b84c773fa4512c421d0157c5b
chaeyun-sim <bysimune@gmail.com>
2022-12-07 21:25:51 +0900

    state and lifecycle

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';  //
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
+import NotificationList from './chapter_06/NotificationList.jsx'
 
-import CommentList from './chapter_05/Comment_list';
+// ReactDOM.render은 react v18부터 사용하지 않는다.
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <NotificationList />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <CommentList />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <NotificationList />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
