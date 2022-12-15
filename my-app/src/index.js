bb5f6ff70bf4860b48514267f1e1957c9c7ec8b2
chaeyun-sim <bysimune@gmail.com>
2022-12-15 14:20:45 +0900

    chap 13-14

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,15 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals.js';
-import NotificationList from './chapter_06/NotificationList.jsx'
-import Accommodate from './chapter_07/Accommodate.jsx'
-import ConfirmButton from './chapter_08/ConfirmButton.jsx';
-import LandingPage from './chapter_09/LandingPage.jsx';
-import AttendanceBook from './chapter_10/AttendanceBook.jsx';
-import SignUp from './chapter_11/SignUp.jsx';
-import Calculator from './chapter_12/Calculator.jsx';
-
+// import NotificationList from './chapter_06/NotificationList.jsx'
+// import Accommodate from './chapter_07/Accommodate.jsx'
+// import ConfirmButton from './chapter_08/ConfirmButton.jsx';
+// import LandingPage from './chapter_09/LandingPage.jsx';
+// import AttendanceBook from './chapter_10/AttendanceBook.jsx';
+// import SignUp from './chapter_11/SignUp.jsx';
+// import Calculator from './chapter_12/Calculator.jsx';
+// import ProfileCard from './chapter_13/ProfileCard.jsx';
+import DarkOrLight from './chapter_14/DarkOrLight.jsx'
 
 // ReactDOM.render은 react v18부터 사용하지 않는다.
 // ReactDOM.render(
@@ -23,7 +24,7 @@ import Calculator from './chapter_12/Calculator.jsx';
 const root = ReactDOMClient.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <DarkOrLight />
   </React.StrictMode>
 );
 
