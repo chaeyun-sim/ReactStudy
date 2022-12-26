3de5bbd4302356b06b28dc8b66c6373e9ebdeef3
chaeyun-sim <bysimune@gmail.com>
2022-12-27 00:16:05 +0900

    how to useEffect()

---
+++ complete-guide/project06-hooks/src/components/Login/Login.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  useEffect(() => {
+    // console.log('Checking form validity!') // key를 입력할때마다 실행됨.
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // clean-up function
+    // setTimeout 외부에 있던 코드와 동일하게 작용
+    return () => {
+      console.log("CLEAN UP")
+      clearTimeout(identifier);  // 가장 최근의 사이드이펙트 함수의 timer 초기화
+      // 새 타이머 설정하기 전에 전의 타이머 삭제
+      // 이부분을 하지 않으면 키를 입력할때마다 CLEANUP과 Checking~ 이 반복되며 나타난다.
+    };
+  }, [enteredEmail, enteredPassword]);
+  // dependencies가 없으면 최초 실행시에 한번만 실행된다.
+  // 값이 바뀔때마다 업데이트하고 싶다 -> dependencies에 값 지정
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    );
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
