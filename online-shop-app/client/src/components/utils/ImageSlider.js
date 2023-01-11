059ba1c9db91aa27c1fe582bac8661e029d625ee
chaeyun-sim <bysimune@gmail.com>
2023-01-11 14:03:30 +0900

    added card and carousel

---
+++ online-shop-app/client/src/components/utils/ImageSlider.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Carousel } from "antd";
+import styles from './ImageSlider.module.css'
+
+const ImageSlider = (props) => {
+    return (
+        <Carousel autoplay>
+            {props.images.map((image, index) => (
+                <div key={index}>
+                    <img className={styles.img} src={`http://localhost:5000/${image}`} />
+                </div>
+            ))}
+        </Carousel>
+    )
+};
+
+export default ImageSlider;
--- online-shop-app/client/src/components/views/LandingPage/LandingPage.js
+++ online-shop-app/client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,60 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import styles from './LandingPage.module.css'
+import { Card, Icon, Col, Row } from "antd";
+import Meta from 'antd/lib/card/Meta';
+import ImageSlider from '../../utils/ImageSlider';
 
 function LandingPage() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.post('/api/product/products')
+        .then((result) => {
+            if (result.data.success){
+                // console.log(result.data)
+                setProducts(result.data.productsInfo)
+            } else {
+                alert("상품을 가져올 수 없습니다.")
+            }
+        })
+    }, []);
+
+    const renderCards = products.map((item, index) => {
+        // console.log(item)
+        return (
+            <Col key={index} lg={6} md={8} xs={12}>
+                <Card cover={<ImageSlider images={item.images} />}>
+                    <Meta
+                        title={item.title}
+                        description={item.description}
+                    />
+                </Card>
+            </Col>
+        )
+    })
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <h2>Let's Travel Anywhere <Icon type="rocket" /> </h2>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            {/* {products.map((item) => {
+
+            })}
+            <Card>
+                <Meta />
+            </Card>
+             */}
+            <Row gutter={[16, 16]}>
+                {renderCards}
+            </Row>
+
+            <div className={styles.button}>
+                <button>더보기</button>
+            </div>
+        </div>
     )
 }
 
