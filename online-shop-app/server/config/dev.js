8771f7afc6069e4f91c19463d310229d0e87f6fa
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-02-07 20:16:35 +0900

    initial commit

--- online-shop-app/server/config/dev.js
+++ online-shop-app/server/config/dev.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//     mongoURI: 'mongodb+srv://chaeyun:zKNNyM0JBBVrMWjy@cluster0.hybtrew.mongodb.net/onlineShop?retryWrites=true&w=majority'
+// }
+
 module.exports = {
-    mongoURI: 'mongodb+srv://chaeyun:zKNNyM0JBBVrMWjy@cluster0.hybtrew.mongodb.net/onlineShop?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://chaeyunsim:C83rGxfHPppwXKUE@cluster0.rq0asub.mongodb.net/?retryWrites=true&w=majority'
 }
