7093599e013eb885c1de529199b9280699e9a4d7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-29 01:11:07 +0900

    app

---
+++ nooks/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>hi</div>
+    </div>
+  );
+}
+
+export default App;
