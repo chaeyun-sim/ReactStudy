6581da164cbcf758f5ac526fd12ab2df32b10695
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-29 23:18:01 +0900

    useNetwork

---
+++ nooks/src/useNetwork/index.js
@@ -0,0 +1,16 @@
+import useNetwork from './useNetwork'
+
+function App() {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline")
+  };
+  const online = useNetwork(handleNetworkChange);
+
+  return (
+    <div className="App">
+      <h1>{online ? "Online" : "Offline"}</h1>
+    </div>
+  );
+}
+
+export default App;
