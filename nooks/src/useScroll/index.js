af40db5faae6972df8faa85f0c381efacb4e9c0a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-03-29 23:18:12 +0900

    useScroll

---
+++ nooks/src/useScroll/index.js
@@ -0,0 +1,13 @@
+import useScroll from './useScroll'
+
+function App() {
+  const { y } = useScroll();
+
+  return (
+    <div className="App" style={{ height: '1000vh'}}>
+      <h1 style={{ position: 'fixed', color: y > 100 ? "red" : "blue"}}>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
